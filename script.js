781a8801886aaef0c91b51205aca16b3e43b13d7
EleonoraDmi <eleonora.dmitrenco@gmail.com>
2024-10-09 12:31:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 let counter = localStorage.getItem('counter') ? parseInt(localStorage.getItem('counter')) : 0;
 
-// Обновляем отображение при загрузке страницы
 updateDisplay();
 
-// Функция для увеличения счетчика
 function incrementCounter() {
-    counter += 1; // Увеличиваем на 1
-    updateDisplay(); // Обновляем отображение
+    counter += 1;
+    updateDisplay();
 }
 
-// Функция для уменьшения счетчика
 function decrementCounter() {
-    counter -= 1; // Уменьшаем на 1
-    updateDisplay(); // Обновляем отображение
-}
+    counter -= 1;
+    updateDisplay();
 
-// Функция для сброса счетчика
 function resetCounter() {
-    counter = 0; // Сбрасываем счетчик
-    updateDisplay(); // Обновляем отображение
+    counter = 0;
+    updateDisplay();
 }
 
-// Функция для обновления отображения счетчика
 function updateDisplay() {
-    document.getElementById('counterDisplay').innerText = counter; // Обновляем текст на странице
-    localStorage.setItem('counter', counter); // Сохраняем значение в localStorage
+    document.getElementById('counterDisplay').innerText = counter;
+    localStorage.setItem('counter', counter);
 }
 
-// Привязываем функции к кнопкам
 document.getElementById('incrementCounter').addEventListener('click', incrementCounter);
 document.getElementById('decrementCounter').addEventListener('click', decrementCounter);
 document.getElementById('resetButton').addEventListener('click', resetCounter);
